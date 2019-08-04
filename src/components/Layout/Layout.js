ea8354ce20695de8f093dee9fd16fecd18d92c6f
Krzysztof <krzysztof.podmokly@gmail.com>
2019-08-04 18:55:45 +0200

    Adding auxiliary component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,8 +4,8 @@ import classes from './Layout.css';
 
 const layout = props => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
+    <div> Toolbar, SideDrawer, Backdrop </div>{' '}
+    <main className={classes.Content}> {props.children} </main>{' '}
   </Aux>
 );
 
