51207e7ae2f8467b5d9f9ca552f1a78ec4bbd669
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-08-05 11:53:34 +0200

    Adding Checkout modal and backdrop

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -23,9 +25,24 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false, // determine if amount of all ingredients is greater than 0
+    purchasing: false // related to modal display
   };
 
+  // Check if there is a possibility to order a burger based on the number of ingredients
+  checkIfPurchasable = ingredients => {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    this.setState({ purchasable: sum > 0 });
+  };
+
+  // Add ingredients to the burger
   addIngredientHandler = type => {
     const oldCount = this.state.ingredients[type];
     const updatedCount = oldCount + 1;
@@ -44,23 +61,64 @@ class BurgerBuilder extends Component {
       totalPrice: updatedPrice,
       ingredients: updatedIngredients
     });
+
+    this.checkIfPurchasable(updatedIngredients);
   };
 
+  // Remove ingredients from the burger
   removeIngredientHandler = type => {
     const oldCount = this.state.ingredients[type];
+    // if (oldCount <= 0) {
+    //   return;
+    // }
     const updatedCount = oldCount === 0 ? 0 : oldCount - 1;
     const updatedIngredients = {
       ...this.state.ingredients
     };
-
     updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+
+    this.checkIfPurchasable(updatedIngredients);
+  };
+
+  // method should be triggered when ORDER button is clicked then Modal is fired
+  purchaseHandler = () => {
+    this.setState({ purchasing: true });
+  };
+
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
   };
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
     return (
       <Aux>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
+          <OrderSummary ingredients={this.state.ingredients} />
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngredientHandler} />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          purchasable={this.state.purchasable}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+          ordered={this.purchaseHandler}
+        />
       </Aux>
     );
   }
